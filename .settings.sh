c5ebb1dfcb4db2c2853b22c79542dd483b1caaaf
Stephen Bridgwater <wizzdm@outlook.com>
2022-03-10 11:36:38 +0000

    create settings
    settings for connecting ionos webserver ssh

---
+++ .settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ionos ssh
+ssh u104701444@access870942105.webspace-data.io
+
+killall sshfs
+
+sshfs u104701444@access870942105.webspace-data.io
+
+sudo umount -l mount
+
+# passwd : @Ku2XETA9F3rq#d
