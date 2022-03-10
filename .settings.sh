bf0116e3a031d46770003b4bb1f5c4b25dc90ce6
Stephen Bridgwater <wizzdm@outlook.com>
2022-03-10 11:41:59 +0000

    Update .settings.sh

--- .settings.sh
+++ .settings.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# ionos ssh
+# AWS Console Login
+# Open Cloud9 IDE
+# https://us-east-1.console.aws.amazon.com/cloud9/ide/ddeeb938fa014a60bdeb6e6af771ba9f
+
+# stop all ssh processes
+sudo killall ssh*
+
+# ionos ssh connection
 ssh u104701444@access870942105.webspace-data.io
 
-killall sshfs
-
-sshfs u104701444@access870942105.webspace-data.io
-
-sudo umount -l mount
-
 # passwd : @Ku2XETA9F3rq#d
+
+# ionos mount
+sshfs u104701444@access870942105.webspace-data.io:/ /mount
+
+
+
+export SPOTIPY_CLIENT_ID='e81c5ecbf0be465d99653d96ce52c046'
+export SPOTIPY_CLIENT_SECRET='598d0f5379014b0f83da2cff1a44c988'
